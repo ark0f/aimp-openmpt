287135c3b229e365513a18de5c7c115d7ec8ff3e
ark0f <arsenylyashenko.3@gmail.com>
2020-08-04 00:42:11 +0300

    Use wrapper for Module

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,76 @@ enum Error {
     Module,
 }
 
+const SAMPLE_RATE: i32 = 48000;
+
+fn seconds_to_bytes(seconds: c_double) -> i64 {
+    (seconds * SAMPLE_RATE as f64 * 2.0 * (32.0 / 8.0)) as i64
+}
+
+fn bytes_to_seconds(bytes: i64) -> c_double {
+    bytes as f64 / (SAMPLE_RATE as f64 * 2.0 * (32.0 / 8.0))
+}
+
+struct ModuleWrapper(Module);
+
+impl ModuleWrapper {
+    fn ctl_set(&mut self, k: &str, v: &str) -> bool {
+        self.0.ctl_set(k, v)
+    }
+
+    fn set_render_stereo_separation(&mut self, percentage: i32) -> bool {
+        self.0.set_render_stereo_separation(percentage)
+    }
+
+    fn set_render_interpolation_filter_length(&mut self, filter_length: i32) -> bool {
+        self.0.set_render_interpolation_filter_length(filter_length)
+    }
+
+    fn pos(&mut self) -> i64 {
+        seconds_to_bytes(self.pos_secs())
+    }
+
+    fn pos_secs(&mut self) -> f64 {
+        self.0.get_position_seconds()
+    }
+
+    fn set_pos_secs(&mut self, secs: f64) -> f64 {
+        self.0.set_position_seconds(secs)
+    }
+
+    fn duration(&mut self) -> i64 {
+        seconds_to_bytes(self.duration_secs())
+    }
+
+    fn duration_secs(&mut self) -> f64 {
+        self.0.get_duration_seconds()
+    }
+
+    fn metadata(&mut self, key: &str) -> Option<String> {
+        self.0.get_metadata(key).filter(|s| !s.is_empty())
+    }
+
+    fn read(&mut self, buf: &mut [u8]) -> i32 {
+        let stereo_len = buf.len() / mem::size_of::<f32>();
+        let mut stereo = unsafe {
+            #[allow(clippy::cast_ptr_alignment)]
+            Vec::from_raw_parts(
+                buf as *mut [u8] as *mut u8 as *mut f32,
+                stereo_len,
+                stereo_len,
+            )
+        };
+
+        let read = self
+            .0
+            .read_interleaved_float_stereo(SAMPLE_RATE, &mut stereo);
+
+        mem::forget(stereo);
+
+        (read * mem::size_of::<f32>() * 2) as i32
+    }
+}
+
 struct OpenMptFormats;
 
 impl FileFormat for OpenMptFormats {
@@ -36,16 +106,6 @@ impl FileFormat for OpenMptFormats {
     const FLAGS: FileFormatsCategory = FileFormatsCategory::AUDIO;
 }
 
-const SAMPLE_RATE: i32 = 48000;
-
-fn seconds_to_bytes(seconds: c_double) -> i64 {
-    (seconds * SAMPLE_RATE as f64 * 2.0 * (32.0 / 8.0)) as i64
-}
-
-fn bytes_to_seconds(bytes: i64) -> c_double {
-    bytes as f64 / (SAMPLE_RATE as f64 * 2.0 * (32.0 / 8.0))
-}
-
 struct OpenMptDecoderBuilder;
 
 impl AudioDecoderBuilder for OpenMptDecoderBuilder {
@@ -55,14 +115,15 @@ impl AudioDecoderBuilder for OpenMptDecoderBuilder {
     type Error = Error;
 
     fn create(&self, mut stream: Stream) -> Result<Self::Decoder, Self::Error> {
-        let mut module =
-            Module::create(&mut stream, Logger::StdErr, &[]).map_err(|()| Error::Module)?;
+        let mut module = Module::create(&mut stream, Logger::StdErr, &[])
+            .map(ModuleWrapper)
+            .map_err(|()| Error::Module)?;
         module.ctl_set("render.resampler.emulate_amiga", "1");
         module.ctl_set("render.resampler.emulate_amiga_type", "a500");
         module.ctl_set("seek.sync_samples", "1");
         module.set_render_interpolation_filter_length(8);
         module.set_render_stereo_separation(200);
-        if let Some(warnings) = module.get_metadata("warnings").filter(|s| !s.is_empty()) {
+        if let Some(warnings) = module.metadata("warnings") {
             log::warn!("Module warnings: {}", warnings);
         }
 
@@ -75,7 +136,7 @@ impl AudioDecoderBuilder for OpenMptDecoderBuilder {
 struct OpenMptDecoder(Mutex<DecoderInner>);
 
 struct DecoderInner {
-    module: Module,
+    module: ModuleWrapper,
 }
 
 impl OpenMptDecoder {
@@ -94,21 +155,20 @@ impl AudioDecoder for OpenMptDecoder {
             guard
                 .sample_rate(SAMPLE_RATE)
                 .channels(2)
-                .duration(module.get_duration_seconds());
+                .duration(module.duration_secs());
 
-            if let Some(title) = module.get_metadata("title") {
+            if let Some(title) = module.metadata("title") {
                 guard.title(title.into());
             }
 
-            if let Some(artist) = module.get_metadata("artist") {
+            if let Some(artist) = module.metadata("artist") {
                 guard.artist(artist.into());
             }
 
             let ty = module
-                .get_metadata("originaltype_long")
-                .filter(|s| !s.is_empty())
-                .or_else(|| module.get_metadata("type_long"));
-            let tracker = module.get_metadata("tracker");
+                .metadata("originaltype_long")
+                .or_else(|| module.metadata("type_long"));
+            let tracker = module.metadata("tracker");
             let codec = match (ty, tracker) {
                 (Some(ty), Some(tracker)) => Some(format!("{} / {}", ty, tracker)),
                 (Some(ty), None) => Some(ty),
@@ -119,11 +179,11 @@ impl AudioDecoder for OpenMptDecoder {
                 guard.codec(codec.into());
             }
 
-            if let Some(comment) = module.get_metadata("message") {
+            if let Some(comment) = module.metadata("message") {
                 guard.comment(comment.into());
             }
 
-            if let Some(date) = module.get_metadata("date") {
+            if let Some(date) = module.metadata("date") {
                 guard.date(date.into());
             }
         }
@@ -156,7 +216,7 @@ impl AudioDecoder for OpenMptDecoder {
     }
 
     fn size(&self) -> i64 {
-        let size = seconds_to_bytes(self.get().module.get_duration_seconds());
+        let size = self.get().module.duration();
         if size == i64::MAX {
             0
         } else {
@@ -165,16 +225,16 @@ impl AudioDecoder for OpenMptDecoder {
     }
 
     fn pos(&self) -> i64 {
-        seconds_to_bytes(self.get().module.get_position_seconds())
+        self.get().module.pos()
     }
 
     fn set_pos(&self, pos: i64) -> bool {
         let module = &mut self.get().module;
-        let secs = bytes_to_seconds(pos);
-        if secs > module.get_duration_seconds() {
+        let pos = bytes_to_seconds(pos);
+        if pos > module.duration_secs() {
             false
         } else {
-            module.set_position_seconds(secs);
+            module.set_pos_secs(pos);
             true
         }
     }
@@ -184,24 +244,7 @@ impl AudioDecoder for OpenMptDecoder {
             return 0;
         }
 
-        let stereo_len = buf.len() / mem::size_of::<f32>();
-        let mut stereo = unsafe {
-            #[allow(clippy::cast_ptr_alignment)]
-            Vec::from_raw_parts(
-                buf as *mut [u8] as *mut u8 as *mut f32,
-                stereo_len,
-                stereo_len,
-            )
-        };
-
-        let read = self
-            .get()
-            .module
-            .read_interleaved_float_stereo(SAMPLE_RATE, &mut stereo);
-
-        mem::forget(stereo);
-
-        (read * mem::size_of::<f32>() * 2) as i32
+        self.get().module.read(buf)
     }
 
     fn buffering_progress(&self) -> Option<BufferingProgress> {
