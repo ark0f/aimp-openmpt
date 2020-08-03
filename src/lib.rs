7b9304edf29c033888cda2cd54096f8b6c814816
Ivan Sychov <60847190+owlnull@users.noreply.github.com>
2020-08-03 23:26:57 +0500

    Update supported formats

--- src/lib.rs
+++ src/lib.rs
@@ -31,8 +31,7 @@ impl FileFormat for OpenMptFormats {
         "*.669", "*.amf", "*.ams", "*.c67", "*.dbm", "*.dtm", "*.far", "*.gdm", "*.ice", "*.st26",
         "*.imf", "*.it", "*.itp", "*.j2b", "*.m15", "*.stk", "*.mdl", "*.med", "*.mo3", "*.mod",
         "*.mptm", "*.mt2", "*.mtm", "*.okt", "*.oxm", "*.plm", "*.psm", "*.pt36", "*.ptm", "*.s3m",
-        "*.sfx", "*.sfx2", "*.mms", "*.stm", "*.stp", "*.ult", "*.umx", "*.wow", "*.xm", "*.midi",
-        "*.wav",
+        "*.sfx", "*.sfx2", "*.mms", "*.stm", "*.stp", "*.ult", "*.umx", "*.wow", "*.xm", "*.as3m",
     ];
     const FLAGS: FileFormatsCategory = FileFormatsCategory::AUDIO;
 }
